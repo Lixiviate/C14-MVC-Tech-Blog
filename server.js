f66b6ddc29e6da865906e0ea7e00ef489984903a
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-08-13 22:46:36 -0600

    Update server.js with boilerplate

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
-
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const { strict } = require('assert');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,11 +29,21 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`,
+    ),
+  );
 });
