5192384794d91493bc92ae170ffdfe740fc491ca
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-08-18 04:24:53 -0600

    Fix: comment failing to add

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,7 +6,7 @@ const withAuth = require('../../utils/auth');
 router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
-      comment_text: req.body.content,
+      comment_text: req.body.comment_text,
       user_id: req.session.user_id,
       post_id: req.body.post_id,
     });
