b30b08903e1288e5143d43e71d6c913e5d6e7a17
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-08-18 21:37:57 -0600

    Fix: use of bcrypt

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,25 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+const bcrypt = require('bcrypt');
 
-// Route to test connection
-// GET all users http://localhost:3001/api/users
-router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-    });
-    res.json(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// POST user login http://localhost:3001/api/users/login Body: { "username": "newuser", "password": "password123" }
+// Login http://localhost:3001/api/users/login Body: { "username": "newuser", "password": "password123" }
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({
       where: { username: req.body.username },
     });
+
     if (!userData) {
       res
         .status(400)
@@ -27,7 +17,11 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password,
+    );
+
     if (!validPassword) {
       res
         .status(400)
@@ -38,6 +32,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.loggedIn = true;
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
   } catch (err) {
@@ -45,8 +40,8 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// POST user logout http://localhost:3001/api/users/logout
-router.post('/logout', (req, res) => {
+// Logout http://localhost:3001/api/users/logout
+router.post('/logout', withAuth, (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -56,17 +51,20 @@ router.post('/logout', (req, res) => {
   }
 });
 
-// POST create a new user http://localhost:3001/api/users/signup Body: { "username": "newuser", "password": "password123" }
+// Signup http://localhost:3001/api/users/signup Body: { "username": "newuser", "password": "password123" }
 router.post('/signup', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const userData = await User.create({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.loggedIn = true;
-      res.status(200).json({
-        user: userData,
-        message: 'You have signed up and are now logged in!',
-      });
+
+      res.status(200).json(userData);
     });
   } catch (err) {
     res.status(400).json(err);
